04b4466bbd9528608204d46cc53a702348007f04
ErikLMZ <eriklima0303@gmail.com>
2024-07-29 13:45:48 -0300

    mudei as pastas de caminho

--- fined.py
+++
@@ -1,7 +0,0 @@
-velocidade = int(input('Informe a velocidade do carro: '))
-if velocidade > 80:
-    print('Você foi multado!')
-    multa = (velocidade - 80) * 7
-    print('Sua multa é de R${:.2f} reais'.format(multa))
-else:
-    print('Você não foi multado!')
