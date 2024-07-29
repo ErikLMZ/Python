3e0da4681daa12fcb58deeacb40e7d928693d807
ErikLMZ <eriklima0303@gmail.com>
2024-07-29 11:58:52 -0300

    alterei o tipo da variavel

--- PriceTravilling.py
+++ PriceTravilling.py
@@ -1,4 +1,4 @@
-km = int(input('Informe a distância do seu destino: '))
+km = float(input('Informe a distância do seu destino: '))
 if km <= 200:
     passagem = km * 0.50
     print('Sua passagem é no valor de R$ {:.2f}'.format(passagem))
