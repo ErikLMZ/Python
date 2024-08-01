4e089bdba0f8f484ba0b46d122e397dd74d6eae2
ErikLMZ <eriklima0303@gmail.com>
2024-08-01 08:29:37 -0300

    altereção

--- Python 2/ConvertionNumTypes.py
+++ Python 2/ConvertionNumTypes.py
@@ -1,15 +1,15 @@
 numero = int(input('Informe um número: '))
 print('''Qual será o tipo de conversão:
 [1] -Binário 
-[2} -Octal 
+[2] -Octal 
 [3] -Hexadecimal''')
 tipo = str(input('')).strip().upper()
 if tipo == "1":
     conversao = bin(numero)
-    print('Seu número convertido para Binário fica: {}'.format(conversao))
+    print('Seu número convertido para Binário fica: {}'.format(conversao[2:]))
 elif tipo == "2":
     conversao = oct(numero)
-    print('Seu número convertido para Octal fica: {}'.format(conversao))
+    print('Seu número convertido para Octal fica: {}'.format(conversao[2:]))
 elif tipo == "3":
     conversao = hex(numero)
-    print('Seu número convertido para Hexadecimal fica: {}'.format(conversao))
+    print('Seu número convertido para Hexadecimal fica: {}'.format(conversao[2:]))
