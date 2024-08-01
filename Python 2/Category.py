f391cf94cc420538c211b747e4f5c26f8a96fe9d
ErikLMZ <eriklima0303@gmail.com>
2024-08-01 09:11:20 -0300

    alteraçõs

--- Python 2/Category.py
+++ Python 2/Category.py
@@ -1,9 +1,11 @@
+from datetime import date
 ano = int(input('Informe seu ano de nascimento: '))
-if 2024 - ano <= 9:
+anoatual = date.today().year
+if anoatual - ano <= 9:
     print('sua categotia é: MIRIM')
-elif 2024 - ano <= 14:
+elif anoatual - ano <= 14:
     print('sua categoria é: INFANTIL')
-elif 2024 - ano <= 19:
+elif anoatual - ano <= 19:
     print('sua categoria é: JUNIOR')
-elif 2024 - ano > 19:
+elif anoatual - ano > 19:
     print('sua categoria é: MASTER')
--- Python 2/RecruitSoldiers.py
+++ Python 2/RecruitSoldiers.py
@@ -1,10 +1,9 @@
-from datetime import datetime
+from datetime import date
 ano = int(input('Informe seu ano de nascimento: '))
-anoatual = datetime.now()
-anoatualfinal = anoatual.year 
-if anoatualfinal - ano == 18:
+anoatual = date.today().year
+if  anoatual - ano == 18:
     print('Você deve se alistar esse ano!')
-elif anoatualfinal - ano < 18:
+elif anoatual - ano < 18:
     print('Você ainda não precisa de alistar!')
-elif anoatualfinal - ano > 18:
+elif anoatual - ano > 18:
     print('Seu tempo de alistamento já passou!')
